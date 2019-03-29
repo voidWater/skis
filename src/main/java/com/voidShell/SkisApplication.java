635e5dc98a90365ecebbd3b1127498a8f7015ecb
voidShell <903458283@qq.com>
2019-03-29 22:29:32 +0800

    init 我们的首次提交

---
+++ src/main/java/com/voidShell/SkisApplication.java
@@ -0,0 +1,14 @@
+package com.voidShell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkisApplication.class, args);
+		System.out.println("213123");
+	}
+
+}
